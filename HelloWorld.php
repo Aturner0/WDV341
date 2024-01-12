98a379e73de6e9ed6bb3e27b73c3122210e1aa84
Aturner0 <143664984+Aturner0@users.noreply.github.com>
2024-01-11 19:03:08 -0600

    Create HelloWorld.php
    Signed-off-by: Aturner0 <143664984+Aturner0@users.noreply.github.com>

---
+++ HelloWorld.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WDV341 Intro PHP</title>
+</head>
+    <style>
+        body {
+            background-color:<?php echo "limegreen";?>;
+        }
+    </style>
+<body>
+    <h1>WDV341 Intro to PHP</h1>
+    <h2>HelloWorld!</h2>
+    <?php
+        echo "<p>HelloWorld</p>";
+    ?>
+
+    <script>
+        document.write("<p>welcome to php</p>");
+        <p>welcome to php</p>
+    </script>
+</body>
+</html>
